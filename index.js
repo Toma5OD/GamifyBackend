f2750ddc4851f45db6b4a02ceeabdc3f358d24b3
Tomas O Dalaigh <tom1996.18@gmail.com>
2023-12-02 18:45:46 +0000

    Initial commit of Node.js backend

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('Hello from Gamify Backend!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
