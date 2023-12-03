0aa90e28de9a40401a8c40f11c11ad641ed6ca4e
Tomas O Dalaigh <tom1996.18@gmail.com>
2023-12-03 22:55:59 +0000

    Layout fix fix

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const userRoutes = require('./routes/users');
+require('dotenv').config();
 
 const app = express();
-require('dotenv').config();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // or app.use(express.json());
 
 // Test Route
 app.get('/', (req, res) => {
     res.send('Hello from Gamify Backend!');
 });
 
+// Routes
+app.use('/api', userRoutes);
+
+// other app settings (if any)
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
